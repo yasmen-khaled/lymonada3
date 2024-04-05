b84eb7bf5c95fb5dba182810b17ab77a696479e0
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-04-05 12:59:21 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,24 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Function to determine manual chunks
 function manualChunks(id) {
- // Example: Group all vendor modules into a single chunk
  if (id.includes('node_modules')) {
     return 'vendor';
  }
 }
 
-export default defineConfig({
- plugins: [react()],
- build: {
-    rollupOptions: {
-      output: {
-        manualChunks: manualChunks,
+export default defineConfig(({ mode }) => {
+ const env = loadEnv(mode, process.cwd(), '')
+ return {
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: manualChunks,
+        },
       },
+      chunkSizeWarningLimit: 10000, 
     },
-    chunkSizeWarningLimit: 10000, 
- },
+    base: './dist',
+ }
 })
